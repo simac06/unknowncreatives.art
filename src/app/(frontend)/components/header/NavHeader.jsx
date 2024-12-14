0db8c8772ccd50ffb3fa97f773a9a5b7c0fc32b6
Simon <75936175+simac06@users.noreply.github.com>
2024-12-14 13:35:35 -0500

    Updates index route to be the homepage, moves linkpage to /link route.

--- src/app/(frontend)/components/header/NavHeader.jsx
+++ src/app/(frontend)/components/header/NavHeader.jsx
@@ -17,11 +17,11 @@ export default function NavHeader({ position }) {
   const navBarPosition =
     position === "absolute" ? "absolute top-0 bg-transparent" : "";
 
-    const menuItems = [
-      { name: "Explore Campaigns", path: "/campaigns" },
-      { name: "Dashboard", path: "/dashboard" },
-      { name: "Link page", path: "/" },
-    ];
+  const menuItems = [
+    { name: "Explore Campaigns", path: "/campaigns" },
+    { name: "Dashboard", path: "/dashboard" },
+    { name: "Link page", path: "/" },
+  ];
 
   // Animation variants for menu items
   const menuAnimations = {
@@ -30,27 +30,30 @@ export default function NavHeader({ position }) {
       y: -50,
       transition: {
         duration: 0.2,
-        delay: i * 0.1 // Stagger effect
-      }
+        delay: i * 0.1, // Stagger effect
+      },
     }),
     visible: (i) => ({
       opacity: 1,
       y: 0,
       transition: {
         duration: 0.5,
-        delay: i * 0.15 // Stagger effect
-      }
-    })
+        delay: i * 0.15, // Stagger effect
+      },
+    }),
   };
 
   return (
-    <Navbar isBordered isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}
+    <Navbar
+      isBordered
+      isMenuOpen={isMenuOpen}
+      onMenuOpenChange={setIsMenuOpen}
       isBlurred={false}
       className={navBarPosition}
     >
       <NavbarContent>
         <NavbarBrand>
-        <Link color="foreground" href="/" className="font-bold text-xl">
+          <Link color="foreground" href="/" className="font-bold text-xl">
             acme
           </Link>
         </NavbarBrand>
@@ -64,33 +67,33 @@ export default function NavHeader({ position }) {
       </NavbarContent>
       <NavbarMenu className="">
         <div className="h-2/3 flex flex-col justify-evenly">
-        {menuItems.map((item, index) => (
-          <motion.div
-            key={`${item}-${index}`}
-            className=""
-            custom={index}
-            initial="hidden"
-            animate={isMenuOpen ? "visible" : "hidden"}
-            variants={menuAnimations}
-          >
-            <NavbarMenuItem>
-              <Link
-                href={item.path}
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === menuItems.length - 1
-                      ? "danger"
-                      : "foreground"
-                }
-                className="w-full text-3xl"
-                size="lg"
-              >
-                {item.name}
-              </Link>
-            </NavbarMenuItem>
-          </motion.div>
-        ))}
+          {menuItems.map((item, index) => (
+            <motion.div
+              key={`${item}-${index}`}
+              className=""
+              custom={index}
+              initial="hidden"
+              animate={isMenuOpen ? "visible" : "hidden"}
+              variants={menuAnimations}
+            >
+              <NavbarMenuItem>
+                <Link
+                  href={item.path}
+                  color={
+                    index === 2
+                      ? "primary"
+                      : index === menuItems.length - 1
+                        ? "danger"
+                        : "foreground"
+                  }
+                  className="w-full text-3xl"
+                  size="lg"
+                >
+                  {item.name}
+                </Link>
+              </NavbarMenuItem>
+            </motion.div>
+          ))}
         </div>
       </NavbarMenu>
     </Navbar>
