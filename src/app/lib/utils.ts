16e125167d8091b3a0e15a9a2097aed8bb894d55
Simon <75936175+simac06@users.noreply.github.com>
2024-12-19 15:04:42 -0500

    Attempt to fix vercel images not loading. Issue with the api url in environment variables

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -1,4 +1,12 @@
 export const getImageUrl = (url: string | undefined): string => {
   if (!url) return "/placeholder-image.jpg";
-  return `${process.env.NEXT_PUBLIC_API_URL}${url}`;
+
+  // Determine the base API URL
+  const baseUrl =
+    process.env.NEXT_PUBLIC_API_URL || // Use custom API URL if defined
+    (process.env.NEXT_PUBLIC_VERCEL_URL
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+      : "http://localhost:3000"); // Default to localhost for development
+
+  return `${baseUrl}${url}`;
 };
